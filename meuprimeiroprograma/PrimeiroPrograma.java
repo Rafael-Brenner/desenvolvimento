c6b944c195b8ba4cafb593860bbdbb44f3985274
rafaelbrenner <rafaelbrenner10@gmail.com>
2023-08-08 19:07:11 -0300

    versão-01

--- meuprimeiroprograma/PrimeiroPrograma.java
+++ meuprimeiroprograma/PrimeiroPrograma.java
@@ -2,7 +2,7 @@ package meuprimeiroprograma;
 
 public class PrimeiroPrograma {
        public static void main(String[] args) {
-        System.out.print("Olá Mundo");
+        System.out.println("Olá Mundo");
         
     }
 }
