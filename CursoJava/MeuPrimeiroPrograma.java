5e15b1f1140021681de1f8bda8e47e9ede61929f
Rafael Brenner <rafaelbrenner10@gmail.com>
2023-08-19 14:59:43 +0000

    teste

---
+++ CursoJava/MeuPrimeiroPrograma.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+//import java.util.Locale;
+
+    public class MeuPrimeiroPrograma{
+
+        public static void main (String[] args){
+
+            Scanner sc = new Scanner(System.in);
+
+            int x = sc.nextInt();
+            System.out.println(x);
+           
+            sc.close();
+            
+        }
+        
+
+    }
